87db09aa48a9ead0ed1292218621fe20a7af2d57
sonicbigboom <sbb-github@potrt.com>
2024-09-13 00:25:55 -0400

    LEM-14: Started the LEMURS api.

---
+++ src/test/java/edu/wpi/lemurs/api/LemursApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wpi.lemurs.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemursApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

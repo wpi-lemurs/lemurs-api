044c33040fc533cdcf9823863fce6533c346060e
atitcombe <86790121+atitcombe@users.noreply.github.com>
2024-09-22 22:46:14 -0400

    Revert "Sets up a basic api and postgres database."

--- src/main/java/edu/wpi/lemurs/api/repositories/DataRepository.java
+++
@@ -1,8 +0,0 @@
-/* Copyright (C) 2024 Worcester Polytechnic University */
-package edu.wpi.lemurs.api.repositories;
-
-import edu.wpi.lemurs.api.entities.data.Data;
-import org.springframework.data.repository.CrudRepository;
-
-/** A {@link CrudRepository} for a {@link DataRepository}. */
-public interface DataRepository extends CrudRepository<Data, Integer> {}

cbac7191c21f9dcff5062b259928fc1a8fdea1df
sonicbigboom <sbb-github@dsgt.com>
2024-10-01 08:50:23 -0400

    LEM-15: Added index for umass id.

--- database/createTables.sql
+++ database/createTables.sql
@@ -1,3 +1,4 @@
+-- Table for users.
 CREATE TABLE app_user (
 	id INT GENERATED ALWAYS AS IDENTITY,
 	umass_id INT NOT NULL,
@@ -7,6 +8,11 @@ CREATE TABLE app_user (
 	UNIQUE (umass_id)
 );
 
+-- Index for finding users based on their umass id.
+CREATE INDEX idx_app_user_umass_id 
+ON app_user (umass_id);
+
+-- Table for generic data.
 CREATE TABLE data (
 	id INT GENERATED ALWAYS AS IDENTITY,
 	type VARCHAR(255) NOT NULL,

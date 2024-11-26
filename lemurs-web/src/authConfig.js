2e6e8637fd5d3c6c8d3655138e3ce31b2cb92b19
sonicbigboom <sbb-github@potrt.com>
2024-11-26 10:02:34 -0500

    Hotfix for web app.

--- lemurs-web/src/authConfig.js
+++ lemurs-web/src/authConfig.js
@@ -55,5 +55,5 @@ export const msalConfig = {
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
 export const loginRequest = {
-    scopes: ["openid", "profile", "email"]
+    scopes: ["b00e7cc0-f93d-4caf-9f9c-c97d8d6f6a0d/profile"]
 };

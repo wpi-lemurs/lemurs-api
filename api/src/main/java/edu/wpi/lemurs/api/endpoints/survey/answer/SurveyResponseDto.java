35b54ee381a691931a6e101c9fb318ab5c012320
Kevin Hickey <khickey@Kevins-MacBook-Pro-9.local>
2025-08-15 19:07:52 -0400

    possible fix to app crashing after survey submission

--- api/src/main/java/edu/wpi/lemurs/api/endpoints/survey/answer/SurveyResponseDto.java
+++ api/src/main/java/edu/wpi/lemurs/api/endpoints/survey/answer/SurveyResponseDto.java
@@ -1,17 +1,25 @@
 /* Copyright (C) 2024 Worcester Polytechnic University */
 package edu.wpi.lemurs.api.endpoints.survey.answer;
 
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class SurveyResponseDto {
-  private Integer id;
-  private List<AnswerDto> answers;
+    @JsonProperty("id")
+    @JsonAlias({"surveyId", "survey_id"})
+    private Long id;
+    
+    @JsonProperty("answers")
+    @JsonAlias({"responses"})
+    private List<AnswerDto> answers;
 }

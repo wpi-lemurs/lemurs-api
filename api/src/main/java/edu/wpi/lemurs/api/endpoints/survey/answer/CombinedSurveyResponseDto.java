7884ba8d63c9513848aceaea72ec9d0b70bfa66a
Kevin Hickey <khickey@Kevins-MacBook-Pro-9.local>
2025-08-15 19:17:37 -0400

    correction to previous commit

--- api/src/main/java/edu/wpi/lemurs/api/endpoints/survey/answer/CombinedSurveyResponseDto.java
+++ api/src/main/java/edu/wpi/lemurs/api/endpoints/survey/answer/CombinedSurveyResponseDto.java
@@ -1,30 +1,19 @@
 /* Copyright (C) 2024 Worcester Polytechnic University */
 package edu.wpi.lemurs.api.endpoints.survey.answer;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Date;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.Instant;
-import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class CombinedSurveyResponseDto {
-    @JsonProperty("timestamp")
-    @JsonAlias({"time", "submissionTime"})
-    private Instant timestamp;
-    
-    @JsonProperty("surveys")
-    @JsonAlias({"responses", "surveyResponses"})
-    private List<SurveyResponseDto> surveys;
-    
-    @JsonProperty("notificationStart")
-    @JsonAlias({"notification_start", "notificationTime"})
-    private Instant notificationStart;
+  private Date timestamp;
+  private List<SurveyResponseDto> surveys;
+  private Date notificationStart;
 }

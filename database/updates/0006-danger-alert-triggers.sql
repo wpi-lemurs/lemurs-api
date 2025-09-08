473359129fc17e12e3b69531a12364da821ea862
Kevin Hickey <khickey@vpnclient-10-217-64-82.int.wpi.edu>
2025-09-08 18:13:01 -0400

    removed 'how strong urge to injure' question from danger alert triggers table

--- database/updates/0006-danger-alert-triggers.sql
+++ database/updates/0006-danger-alert-triggers.sql
@@ -17,7 +17,6 @@ VALUES
   (5, 2, 'Suicide preparation (score: {score}/5 on question ''Considered a specific suicide method or made preparations for your death'').', TRUE),
   (7, 2, 'Intense desire to kill oneself (score: {score}/5 on question ''How intense was your desire to kill yourself?'').', TRUE),
   (8, 2, 'Intent to kill oneself right now (score: {score}/1 on question ''Do you intend to kill yourself right now?'').', TRUE),
-  (9, 2, 'Urge to injure self without intent to die (score: {score}/5 on question ''How strong was your urge to injure yourself without any intent to die?'').', TRUE),
   (11, 2, 'Injured self without intent to die (score: {score}/1 on question ''Injured yourself without any intent to die'').', TRUE),
   (12, 2, 'Attempted suicide (score: {score}/1 on question ''Have you attempted suicide?'').', TRUE),
   (13, 2, 'Severe injury or medical intervention required (score: {score}/1 on question ''Were you severely injured or did you require medical intervention?'').', TRUE);

b952859680c5c157048971f0d5bf7b4554b2e500
Eric Li <ericlibeibei@gmail.com>
2025-09-25 16:29:50 -0400

    Added Endpoints and related files for Calorie data. Tested via postman on the expected DTO from Lemurs App.Ran SpotlessApply

---
+++ api/src/main/java/edu/wpi/lemurs/api/endpoints/health/calorie/CalorieRepository.java
@@ -0,0 +1,6 @@
+/* Copyright (C) 2025 Worcester Polytechnic University */
+package edu.wpi.lemurs.api.endpoints.health.calorie;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CalorieRepository extends CrudRepository<Calorie, Integer> {}

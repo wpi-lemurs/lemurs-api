44b705a83a71a93123a964adf35fe3e39e5c435c
woosterum <84195597+woosterum@users.noreply.github.com>
2025-10-24 17:21:37 -0400

    Update Dashboard.jsx
    fix dashboard height

--- web/src/pages/Dashboard.jsx
+++ web/src/pages/Dashboard.jsx
@@ -12,7 +12,7 @@ export default function Dashboard() {
             ) : (
                 <iframe
                     src="/dashboard/"
-                    style={{ width: '100%', height: '100%', border: 'none' }}
+                    style={{ width: '100%', height: '100vh', border: 'none' }}
                     title="Dashboard"
                 />
             )}

312c0f5b9f58f5533254347c012f85cf67109694
woosterum <84195597+woosterum@users.noreply.github.com>
2025-10-24 17:11:42 -0400

    Update PageLayout.jsx
    fix name

--- web/src/components/PageLayout.jsx
+++ web/src/components/PageLayout.jsx
@@ -20,6 +20,11 @@ export const PageLayout = (props) => {
     const { token } = useContext(TokenContext);
     const { accounts } = useMsal();
 
+    let firstName = "";
+    if (accounts.length > 0 && accounts[0].name) {
+        firstName = accounts[0].name.split(',')[1].trim();
+    }
+
     return (
         <>
             <Navbar bg="primary" variant="dark" className="navbarStyle">
@@ -38,15 +43,14 @@ export const PageLayout = (props) => {
                         <SignInButton setToken={props.setToken}/>
                     ) : (
                         <>
-                            {accounts.length > 0 && (
+                            {firstName && (
                                 <span style={{ color: "white", marginRight: "15px", alignSelf: "center" }}>
-                                    Welcome, {accounts[0].name}
+                                    Welcome, {firstName}
                                 </span>
                             )}
                             <SignOutButton setToken={props.setToken} />
                         </>
                     )}
-
                 </div>
             </Navbar>
             <div className="content">
